77d54f085704a6779c2a22c89c2a82fe51f756c5
Juan SB <juansb.dev@gmail.com>
2023-05-02 17:37:27 -0300

    clase

--- arboles.js
+++ arboles.js
@@ -1,3 +1,30 @@
 // condiciones estructura
 //monoparental - unidireccional - prohibidos los nodos no conexos
 // es arbol si cada nodo cumple las condiciones de estructura
+//caada nodo es un nuevo arbol, x eso no es una linked list
+
+//ARBOL BINARIO como max dos hijos
+// de busqueda: los valores mas altos q su root a la derecha y menores a la izq
+//Balanceado cuando ambas ramas no difieren en mas de un nivel 
+
+//pre order primero nodo => izq der
+
+
+
+let BinarySearchTree = function(value){
+    this.value = value;
+    this.right = null;
+    this.left = null;
+}
+BinarySearchTree.add = function(value){
+    let newNode = new BinarySearchTree(value);
+    if(value <= this.value){
+
+    }
+}
+newTree = new BinarySearchTree();
+newTree.right = new BinarySearchTree(1);
+newTree.add(20)
+
+
+console.log(newTree)
