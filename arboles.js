04739f58a42ea1458605917c5d4301239df480ec
Juan SB <juansb.dev@gmail.com>
2023-05-03 13:43:08 -0300

    fin arboles

--- arboles.js
+++ arboles.js
@@ -41,13 +41,12 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree( data){
-   this.data = data;
+function BinarySearchTree( value){
+   this.value = value;
    this.left = null;
    this.right = null;
 }
 BinarySearchTree.prototype.size = function(){
-   let counter = 0;
       if(!this.left && !this.right) return 1
       if (this.left && !this.right)return this.left.size() + 1;
       if(!this.left && this.right) return this.right.size()+ 1;
@@ -55,7 +54,7 @@ BinarySearchTree.prototype.size = function(){
 }
 
 BinarySearchTree.prototype.insert = function(value) {
-   if (value <= this.data) {
+   if (value <= this.value) {
      if (!this.left) {
        this.left = new BinarySearchTree(value);
      } else {
@@ -75,15 +74,15 @@ BinarySearchTree.prototype.insert = function(value) {
 
 
 BinarySearchTree.prototype.contains = function(value) {
-   if (value === this.data) {
+   if (value === this.value) {
      return true;
-   } else if (value < this.data) {
+   } else if (value < this.value) {
      if (!this.left) {
        return false;
      } else {
        return this.left.contains(value);
      }
-   } else if (value > this.data) {
+   } else if (value > this.value) {
      if (!this.right) {
        return false;
      } else {
@@ -91,18 +90,21 @@ BinarySearchTree.prototype.contains = function(value) {
      }
    }
  }
+
+
 //  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
 BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
-   let aux = [];
-   if(!order || order === 'in-order'){
-     if(this.left){
-       this.left.depthFirstForEach(cb, order);
-     }
-     cb(this.data);
+   if( order === "in-order" || !order){
+     
+    if(this.left){
+      this.left.depthFirstForEach(cb, order);
+      cb(this.value);
+      this.right.depthFirstForEach(cb, order);  }
+
      if(this.right){
-       this.right.depthFirstForEach(cb, order);
-     }
+       this.right.depthFirstForEach(cb, order); }
    }
+
    else if(order === 'post-order'){
      if(this.left){
        this.left.depthFirstForEach(cb, order);
@@ -110,10 +112,10 @@ BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
      if(this.right){
        this.right.depthFirstForEach(cb, order);
      };
-     cb(this.data);
+     cb(this.value);
    }
    else{
-     cb(this.data);
+     cb(this.value);
      if(this.left){
        this.left.depthFirstForEach(cb, order);
      }
@@ -123,22 +125,16 @@ BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
    }
  }
  
- BinarySearchTree.prototype.breadthFirstForEach = function(cb) {
-   let currentLevel = [this];
-   while (currentLevel.length > 0) {
-     let nextLevel = [];
-     currentLevel.forEach(function(node) {
-       cb(node.data);
-       if (node.left) {
-         nextLevel.push(node.left);
-       }
-       if (node.right) {
-         nextLevel.push(node.right);
-       }
-     });
-     currentLevel = nextLevel;
-   }
- };
+ BinarySearchTree.prototype.breadthFirstForEach = function(cb, array = []) {
+  cb(this.value);
+  if(this.left){
+    array.push(this.left); }
+  if(this.right){
+    array.push(this.right) }
+  if(array.length){
+    array.shift().breadthFirstForEach(cb, array)
+  }
+};
 
 
 
@@ -148,4 +144,3 @@ BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
 module.exports = {
    BinarySearchTree,
 };
-
