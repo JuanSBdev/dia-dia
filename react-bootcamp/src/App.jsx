fa6ecca9673b282cfec07403881f0df3acbebafd
Juan SB <juansb.dev@gmail.com>
2023-05-17 14:52:14 -0300

    rickand - react app - vite - hooks

--- react-bootcamp/src/App.jsx
+++ react-bootcamp/src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from '../data';
+// import characters, { Rick } from '../data';
+import Nav from './components/Nav';
+import { useState } from 'react';
 
 function App() {
+   const [characters, setCharacters] = useState([])
+   const example = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      origin: {
+         name: 'Earth (C-137)',
+         url: 'https://rickandmortyapi.com/api/location/1',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+   }
+   const onSearch = data => {
+      setCharacters([...characters, example])
+   }
    return (
       <div className='App'>
-         <SearchBar  onSearch={(characterID) => console.log(characterID)} />
+         <Nav  onSearch={onSearch} />
          <Cards characters={characters} />
       </div>
    );
--- react-bootcamp/src/components/SearchBar.jsx
+++ react-bootcamp/src/components/SearchBar.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css'
 export default function SearchBar(props) {
+   const [id, setId] = useState('')
+   const handleChange = (event)=>{
+      console.log(id)
+      let { value } = event.target;
+      setId( value);
+   }
    return (
       <div className={styles.container} >
-         <input className={styles.input} placeholder='Ingresar valor..' type='search' />
-         <button className={styles.button} onClick={props.onSearch}>+</button>
+         <input className={styles.input}
+          placeholder='Ingresar valor..'
+           type='search'
+            name='search'
+            onChange={handleChange}
+            />
+         <button className={styles.button} onClick={()=> props.onSearch(id)}>+</button>
       </div>
    );
 }
