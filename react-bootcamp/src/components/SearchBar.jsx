24b15ed568fcded5cac8ba7a490648c4a6413a5e
Juan SB <juansb.dev@gmail.com>
2023-05-24 16:37:01 -0300

    searcbar nav resuelta

--- react-bootcamp/src/components/SearchBar.jsx
+++ react-bootcamp/src/components/SearchBar.jsx
@@ -7,6 +7,7 @@ export default function SearchBar(props) {
    const { pathname } = useLocation();
    const isAbout = pathname === '/abt';
    const isHome = pathname === '/home';
+   const isFav = pathname ==='/fav';
    const [id, setId] = useState('');
    const handleChange = (event)=>{
       let { value } = event.target;
@@ -17,46 +18,25 @@ export default function SearchBar(props) {
       <div className={styles.container} >
          <div className={styles.containerLeft}>
          
-         { !isHome && 
-            
-            (
-            <NavLink to={'/home'} >
-            <button className={styles.btnHome} >
-               <img src={btn_imgHome} alt="" />
-            </button>
-            </NavLink>)
-            
-          
-
-         }
-         { isHome && !isAbout &&
-             <NavLink to={'/fav'}>
-             <button className={styles.btnFav} >
-                <img className={styles.imgFav} src={btn_img2} alt="" />
-             </button>
-           </NavLink>
-         }
-         { !isAbout &&
-            (
-            <NavLink to={'/abt'} >
-               <button className={styles.btnAbt} >
-                  <img className={styles.btnAbtImg} src={abt_img} alt="About" />
-               </button>
-            </NavLink>
-            
-
-            )  
-           
-         }
-          { isAbout &&
-             <NavLink to={'/fav'}>
-             <button className={styles.btnFav} >
-                <img className={styles.imgFav} src={btn_img2} alt="" />
-             </button>
-           </NavLink>
-         }
-       
-          
+                {!isAbout &&(<NavLink to={'/abt'} >
+                   <button className={styles.btnAbt} >
+                      <img className={styles.btnAbtImg} src={abt_img} alt="About" />
+                   </button>
+                </NavLink>)}
+               {
+                  isAbout &&(
+                     <NavLink to={'/home'} >
+                   <button className={styles.btnAbtActive} >
+                      <img className={styles.btnAbtImg} src={abt_img} alt="About" />
+                   </button>
+                </NavLink>
+                  )
+               }
+               <NavLink to={'/home'} >
+                  <button className={styles.btnHome} >
+                     <img src={btn_imgHome} alt="" />
+                  </button>
+               </NavLink>
          </div>
           <div className={styles.ContainerRight}>
             <label > Buscar por ID:</label>
@@ -67,7 +47,23 @@ export default function SearchBar(props) {
             onChange={handleChange}
             />
             <button className={styles.button} onClick={()=> props.onSearch(id)}>🔎</button>
-
+            {
+               !isFav &&(
+                  <NavLink to={'/fav'}>
+                  <button className={styles.btnFav} >
+                     <img className={styles.imgFav} src={btn_img2} alt="" />
+                  </button>
+                </NavLink>
+               )
+             }{
+               isFav &&(
+                  <NavLink to={'/home'}>
+                  <button className={styles.btnFavActive} >
+                     <img className={styles.imgFav} src={btn_img2} alt="" />
+                  </button>
+                </NavLink>
+               )
+             }
           </div>
       </div>
    );
