d12ed5a8e563fe47578d6109aab9c625da5c15b0
Juan SB <juansb.dev@gmail.com>
2023-05-23 22:45:48 -0300

    mejora form validate

--- react-bootcamp/src/components/form/validate.js
+++ react-bootcamp/src/components/form/validate.js
@@ -1,21 +1,33 @@
 import React from 'react'
 
 export default function validate(input) {
-    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    const regexPass =new RegExp('[0-9]');
+        let regexMail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        const regexPass = new RegExp('[0-9]');
     const error = {}
   
     if(!input.email){
         error.email = "agrega un email"
     }
+    if(!regexMail.test(input.email)){
+        error.email = "agrega un email válido"
+    }
     if(!input.email.length > 35){
         error.email = 'mayor a 35 caracteres'
-    }  
+    }
+    if(regexMail.test(input.email)){
+        error.email = "prueba con ejemplo@gmail.com "
+    }
     if(!input.password){
         error.password = 'Ingrrese un pass'
     }
+    if(input.password.length <= 3 ){
+        error.password = 'Ingrrese un pass válido'
+    }
     if(!regexPass.test(input.password)){
         error.password = 'almenos un numero'
+    }
+    if(regexPass.test(input.password) &&  input.password.length > 3){
+        error.password = 'Intenta con 123'
     }
     return error;
 
