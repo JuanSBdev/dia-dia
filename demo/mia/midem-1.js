62402ccfce622a169a22f15bb38282013ff73f1c
Juan SB <juansb.dev@gmail.com>
2023-05-29 20:55:08 -0300

    demo de node, lunes

---
+++ demo/mia/midem-1.js
@@ -0,0 +1,6 @@
+
+const {saludo, print} = require ('./midem-2')
+
+console.log('Hola papaaaaa el 1');
+
+print('asasd')
---
+++ demo/mia/midem-2.js
@@ -0,0 +1,8 @@
+
+
+const saludo = 'midem 2';
+const print = (msj)=>console.log(msj)
+console.log(saludo);
+
+module.export = { saludo, print };
+
