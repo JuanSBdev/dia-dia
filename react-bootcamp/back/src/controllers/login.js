09a51d95bfd71b1419161a396192251bddc10814
Juan SB <juansb.dev@gmail.com>
2023-06-07 13:03:13 -0300

    login solved

--- react-bootcamp/back/src/controllers/login.js
+++ react-bootcamp/back/src/controllers/login.js
@@ -1,7 +1,7 @@
 let datos = require('../utils/users');
 
 let login = function (req, res) {
-  let{ email, password }= req.query;
+  let{ email, password } = req.query;
     let access = false;
     datos.forEach((dato) =>{
         if(dato.email === email && dato.password === password){
--- react-bootcamp/back/src/routes/index.js
+++ react-bootcamp/back/src/routes/index.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const { getCharById } = require('../controllers/getCharById');
 const { getCharDetail } = require('../controllers/getCharDetail');
+const { login } = require('../controllers/login');
 
 
 const router = Router();
 
 router.get('/onsearch/:id', getCharById)
 router.get('/detail/:detailId', getCharDetail)
-
+router.use('/login', login)
 
 
 
