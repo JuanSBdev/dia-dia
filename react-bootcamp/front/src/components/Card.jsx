b3d0bb897796c7d3e0f1d670830cef7cebac17b5
Juan SB <juansb.dev@gmail.com>
2023-09-27 00:25:26 -0300

    fix cards

--- react-bootcamp/front/src/components/Card.jsx
+++ react-bootcamp/front/src/components/Card.jsx
@@ -53,12 +53,14 @@ export default function Card(props) {
          <button className={styles.button} onClick={handleOnClose}>X</button>
          </div>
          <NavLink to={`/detail/${id}`} className={styles.link}>
-            <h2 className={styles.nombre}>{props.name}</h2>
             <img className={styles.characterImg} src={props.image} alt='RickImage' />
-            <h2>&#x2764; {props.status}</h2>
-            <h2>{props.species}</h2>
-            <h2>{props.gender}</h2>
-            <h2>{props.origin} asd</h2>
+            <div className={styles.textInfo}>
+            <h2 className={styles.nombre}>{props.name}</h2>
+               {/* <h2>&#x2764; {props.status}</h2>
+               <h2>{props.species}</h2>
+               <h2>{props.gender}</h2>
+               <h2>{props.origin} asd</h2> */}
+               </div>
          </NavLink>
       </div>
    );
